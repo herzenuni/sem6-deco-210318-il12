9909c7e2c48e74f1bf187ce1a42d69236fe21a84
il12 <il5498@yandex.ru>
2018-06-21 11:37:19 +0300

    Update once.py

--- once.py
+++ once.py
@@ -1,18 +1,25 @@
 import functools
 
 def once(func):
-
+    result = 0;
     @functools.wraps(func)
     def inner(*args, **kwargs):
-        if not inner.called:
-            inner.called = True
-            return func(*args, **kwargs)
-    inner.called = False
+        nonlocal result
+        print(inner.called, inner.deco)
+        if not inner.called and not inner.deco:
+          result = func(*args, **kwargs)
+          inner.called = True
+          return(func(*args, **kwargs))
+        else:
+          return(result)
+    inner.called = False  
+    inner.deco = False
     return inner
 
 @once
-def initialize_settings(txt):
-    print("Settings initialized")
-    return(txt)
+def initialize_setting(txt):
+  print("Settings initialized")
+  return(txt)
 
-print(initialize_settings())
+print(initialize_setting(11))
+print(initialize_setting(12))
