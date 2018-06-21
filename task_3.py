0ba3183557e9575e47b175ea90a4927280597f17
il12 <il5498@yandex.ru>
2018-06-21 16:58:34 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,14 +1,5 @@
 import types
 import functools
-def copy_func(f):
-    g = types.FunctionType(
-      f.__code__, 
-      f.__globals__, 
-      argdefs=f.__defaults__, 
-      closure=f.__closure__
-    )
-    g.__kwdefaults__ = f.__kwdefaults__
-    return g
 
 def once(func):
     @functools.wraps(func)
